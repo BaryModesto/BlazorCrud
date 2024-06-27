33db76747b2ca9e9d469ef82563d0cbdc82531a6
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-27 00:08:41 -0300

    Agregar archivos de proyecto.

---
+++ Modelo/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrud.Modelo
+{
+    public class Libro
+    {
+        [Key]
+        public int id { get; set; }
+        [Required(ErrorMessage = "El Titulo es obligatorio")]
+        public string titulo { get; set; }
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public string descripcion{ get; set; }
+        [Required(ErrorMessage = "El autor es obligatorio")]
+        public string autor{ get; set; }
+        [Required(ErrorMessage = "La cantidad de paginas es obligatorio")]
+        public int paginas{ get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public int precio { get; set; }
+        public DateTime fecha_creacion { get; set; }
+    }
+}
