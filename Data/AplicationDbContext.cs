f4e9f9a1cf39fff8829e0da1fa7d02cb7ee8bc4f
Alvaro Alfonso Rodriguez <123104080+BaryModesto@users.noreply.github.com>
2024-06-27 12:41:25 -0300

    feat: Archivo de contexto

--- Data/AplicationDbContext.cs
+++ Data/AplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace BlazorCrud.Data
+﻿using BlazorCrud.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Data
 {
     public class AplicationDbContext : DbContext
     {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base (options)
+        {
+            
+        }
+        public DbSet<Libro>? Libro {  get; set; }
     }
 }
